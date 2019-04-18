b5aeab8e3d304af6ed576d6d49b3a6d61e95242f
Michael Boyko <samaranin2912@gmail.com>
2019-04-18 16:16:44 +0300

    finished base movie data parsing, fixed few bugs

--- IMDBParser.py
+++ IMDBParser.py
@@ -4,7 +4,8 @@ import requests
 from bs4 import BeautifulSoup as Bs
 
 # define same headers for all requests
-HEADERS = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0"}
+HEADERS = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0",
+           "Accept-Language": "en-US,en;q=0"}
 
 
 class StringProcessing:
@@ -31,21 +32,43 @@ class StringProcessing:
         tags_data = text.strip().split(",") if text.strip().split(",") is not None else ""
         return [tag.strip() for tag in tags_data]
 
+    @staticmethod
+    def concatenate_strings(string_list, separator=", "):
+        """
+        Concatenates list of words into one string wits words separated with comas
+        :param string_list: list of strings
+        :param separator: separates words in string
+        :return: single string with word separated with comas
+        """
+        concatenation = ''
+        for list_item in string_list:
+            concatenation += list_item + separator
+
+        # remove redundant coma in the end
+        concatenation = concatenation.strip(', ')
+        return concatenation
+
 
 class Response:
     """
     Helper class for saving and representing responses from server
     """
 
-    def __init__(self, response_status, request_url, request_results):
+    def __init__(self, response_status, request_url, request_result):
         """
         :param response_status: HTML response code (200, 400, etc.)
         :param request_url: request destination
-        :param request_results: parsed and processed response from server (None for empty object or any error)
+        :param request_result: parsed and processed response from server (None for empty object or any error)
         """
         self.response_status = response_status
         self.request_url = request_url
-        self.request_results = request_results
+        self.request_result = request_result
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def __repr__(self):
+        return str(self.__dict__)
 
 
 class Movie:
@@ -74,6 +97,12 @@ class Movie:
 
         for (prop, value) in defaults.items():
             setattr(self, prop, kwargs.get(prop, value))
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def __repr__(self):
+        return str(self.__dict__)
 
 
 class IMDBSearch:
@@ -129,9 +158,15 @@ class IMDBSearch:
         if request.status_code == 200:
             soup = Bs(request.content, 'html.parser')  # get page data
 
-            # get movie title
             title_tag = soup.find('h1', attrs={'class': ''})
-            title = StringProcessing.remove_text_in_brackets(title_tag.text)
+
+            if title_tag is not None:
+                pass
+            else:
+                title_tag = soup.find('div', attrs={'class': 'title_wrapper'}).find('h1', attrs={'class': 'long'})
+
+            # get movie title
+            title = StringProcessing.remove_text_in_brackets(title_tag.text) if title_tag is not None else ''
 
             # get movie release year
             year_tag = title_tag.find('a', href=True)
@@ -143,6 +178,12 @@ class IMDBSearch:
             # dict of lambda-functions for parsing movie sub info
             # return order: rating, duration, genre, launch_date
             sub_info_parsers = {
+                1: lambda sub_info_data: (
+                    '',
+                    '',
+                    StringProcessing.split_words(sub_info_data[0]),
+                    ''
+                ),
                 2: lambda sub_info_data: (
                     '',
                     '',
@@ -171,16 +212,54 @@ class IMDBSearch:
 
             # get imdb rating
             imdb_rating = soup.find('span', attrs={'itemprop': 'ratingValue'})
-            imdb_rating = imdb_rating.text if imdb_rating is not None else ''
+            imdb_rating = imdb_rating.text.strip() if imdb_rating is not None else ''
 
-            print(title + " " + year)
-            print(rating + " " + duration + " " + str(genre) + " " + launch_date)
-            print(image_link)
-            print(imdb_rating)
-            print()
+            # get summary info
+            summary = soup.find('div', attrs={'class': 'plot_summary'})
 
+            # if we do not have summary - make fields empty
+            if summary is None:
+                director, writers, actors, storyline = '', '', '', ''
+            else:
+                # get storyline from summary
+                storyline = summary.find('div', attrs={'class': 'summary_text'})
+                storyline = storyline.text.strip().replace("...\n                    See full summary\xa0", "") \
+                    if storyline is not None else ''
+
+                # find all stuff
+                stuff = summary.find_all('div', attrs={'class': 'credit_summary_item'})
+
+                def persons_parsers(stuff_object):
+                    """
+                    Parses staff and creates single line
+                    :param stuff_object: parsed stuff data
+                    :return: string with stuff name separated with comas
+                    """
+                    stuff_list = stuff_object.find_all('a', href=True)
+                    # create list of persons names
+                    # link to person's name contains 'name' part - so we need only it
+                    # same thing will be for
+                    persons = [
+                        stuff_item.text if 'name' in stuff_item['href'] else ''
+                        for stuff_item in stuff_list
+                        if stuff_list is not None
+                    ]
+                    stuff_string = StringProcessing.concatenate_strings(persons)
+                    stuff_string = stuff_string if stuff_string != 'IMDbPro' else ''
+                    return stuff_string
+
+                # get directors from stuff
+                director = persons_parsers(stuff[0]) if len(stuff) > 0 else ''
+
+                # get writers from stuff
+                writers = persons_parsers(stuff[1]) if len(stuff) > 1 else ''
+
+                # get actors from stuff
+                actors = persons_parsers(stuff[2]) if len(stuff) > 2 else ''
+
+            # creating new movie object to return
             movie = Movie(
-                title=title,
+                name=title,
                 year=year,
                 rating=rating,
                 duration=duration,
@@ -188,6 +267,10 @@ class IMDBSearch:
                 launch_date=launch_date,
                 image_link=image_link,
                 imdb_rating=imdb_rating,
+                director=director,
+                writers=writers,
+                actors=actors,
+                storyline=storyline
             )
 
             # return a Response object with Movie data in
@@ -199,17 +282,18 @@ class IMDBSearch:
 
 if __name__ == "__main__":
 
-    search_text = "Bumblebee"
+    search_text = "Avengers"
 
     imdb_search = IMDBSearch(HEADERS)  # create parser object
 
     search_result = imdb_search.imdb_movie_search(search_text)  # make search request to IMDB
-    search_links = search_result.request_results  # get list of links from response
+    search_links = search_result.request_result  # get list of links from response
 
     if search_links is not None:
         # parsing movie page for every link
         for link in search_links:
-            imdb_search.parse_movie_data_from_imdb(link)
+            response = imdb_search.parse_movie_data_from_imdb(link)
+            print(response.request_result.__dict__)
     else:
         # or show an error
         print("No results found on request \"" + search_result.request_url
